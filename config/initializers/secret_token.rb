58d2242b5ad6c7da9a1a2830f4d46388061464d3
oscarparamo <oscar.paramo@me.com>
2014-02-22 01:57:16 -0600

    Initialized rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Housecall::Application.config.secret_key_base = '9ed9a731bebe314ee3f7286428c2373fa7f1cdc0f120d72d62476098a807d9f2baa07403e5ed501df79a6b800cfb3d3e9e72ddd1c424a2f9b9b06005fb666ee0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Housecall::Application.config.session_store :cookie_store, key: '_housecall_session'
